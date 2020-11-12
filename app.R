093da65c2e71250e9a3a93a9d76301d2c31e33df
Steven Ge <xijin.ge@sdstate.edu>
2020-11-12 15:19:55 -0600

    Add files via upload

---
+++ app.R
@@ -0,0 +1,74 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Read in data files
+# data wrangling
+# filter, tranform
+
+# global variables
+x    <- faithful[, 2]
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            
+            selectInput("select", 
+                        h3("Select a color"), 
+                        choices = c("red","green", "purple","yellow"), 
+                        selected = "yellow"
+            ),
+            
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+            ),
+        
+
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")  ,
+           plotOutput("qqPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = input$select, border = 'white')
+    })
+ 
+    output$qqPlot <- renderPlot({
+
+        qqnorm(x, main = "QQ plot")
+        qqline(x)
+
+    })  
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
